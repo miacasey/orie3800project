bd29b4fff86c4191281d4f8e8951718294a46562
Cayla Hamann <caylahamann@dhcp-gs-9139.eduroam.cornell.edu>
2019-05-06 14:16:32 -0400

    changes

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ N=5
 
 def V(gamma, clicks, left):
     max_bid= 0
-    max_expectation= 0
-    if left==0:
+    max_expectation= -100
+    bid_dict={}
+    if left<1:
         return 0
     else:
         for bid in range(0,101,1):
             expectation=0
-            b=bid/10
-            p1= 1-(b/10)
-            p2= (alpha+gamma)/(alpha+beta+clicks)
-            p3= (b/10)-p2
+            b=float(bid/10)
+            ##alpha = alpha + gamma
+            ##beta = beta + clicks-gamma
+            x= (alpha+gamma)/(alpha+beta+clicks)
+            p1= 1-(float(b/10))
+            p2= float(b/10)*(1-x)
+            p3= x*float(b/10)
             expectation+= p1*V(gamma,clicks,left-1)
-            expectation+= p2*(V(gamma,clicks+1,left-1)-b/2)
-            expectation+= p3*(V(gamma+1,clicks+1,left-1)+10-b/2)
+            expectation+= p2*(V(gamma,clicks+1,left-1)-float(b/2))
+            expectation+= p3*(V(gamma+1,clicks+1,left-1)+10-float(b/2))
+            print(expectation)
             if (expectation>max_expectation):
                 max_expectation= expectation
                 max_bid= b
     return max_bid
 
-print(V(0,10,1))
+print(V(5,5,3))
