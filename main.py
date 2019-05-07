5d268f0907d7773ba25d66a1a9a7f7bc35031a04
Cayla Hamann <ch629@cornell.edu>
2019-05-07 14:18:59 -0400

    this is tuff stuff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 alpha=1
 beta=1
-N=3
+N=40
 
 # bid_dict={}
 # max_expectation=0:
@@ -40,31 +40,29 @@ for l in range(1,N+1):
                 p3= (b/10)*x
                 p2= (b/10)*(1-x)
                 # expectation = p1 + p2*(-b/2) + p3*(10-b/2)
-                expectation= p1*max_bids[l-1,c,g] + p2*(max_bids[l-1,c+1,g]-b/2) + p3*(max_bids[l-1,c+1,g+1]+10-b/2)
+                expectation= p1*max_bids[l-1,c,g] + p2*(max_bids[l-1,c+1,g]-b/2) + p3*(max_bids[l-1,g+1,c+1]+10-b/2)
                 if expectation > max_exp:
                     max_exp = expectation
                     max_bid= b
-                    max_bids[g,c,l]= max_bid
-                    max_exps[g,c,l]= max_exp
+                    max_bids[l,c,g]= max_bid
+                    max_exps[l,c,g]= max_exp
 # print(max_bids)
 print(max_exps)
-print(max_exps[0,0,1])
-print(max_exps[1,1,1])
-print(max_exps[2,2,1])
-print(max_exps[3,3,1])
-print(max_exps[4,4,1])
+
+
+
 
 
 avg_x = []
 avg_y = [] 
 for n in range(1,N+1): 
     avg_x.append(n)
-    avg_y.append(max_exps[n-1,n-1,1]/n)
+    avg_y.append(max_exps[n,0,0]/(N-n+1))
 
-print(avg_x)
+# print(avg_x)
 print(avg_y)
-# plt.plot(avg_x, avg_y)
-# plt.show()
+plt.plot(avg_x, avg_y)
+plt.show()
 
 
 ##need to keep track of largest expected value
