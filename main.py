26d5542a60b5467856bc79b446de256006a36fae
miacasey <miacasey98@outlook.com>
2019-05-07 10:43:51 -0400

    number 6

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import operator
+import matplotlib.pyplot as plt
+
 
 alpha=1
 beta=1
@@ -43,7 +45,7 @@ for l in range(1,N+1):
                     p3= (b/10)*x
                     p2= (b/10)*(1-x)
                     # expectation = p1 + p2*(-b/2) + p3*(10-b/2)
-                    expectation= p1*max_bids[g,c,l-1] + p2*(max_bids[g,c+1,l-1]-b/2) + p3*(max_bids[g+1,c+1,l-1]+10-b/2)
+                    expectation= p1*max_bids[l-1,c,g] + p2*(max_bids[l-1,c+1,g]-b/2) + p3*(max_bids[l-1,c+1, g+1]+10-b/2)
                     if expectation > max_exp:
                         max_exp = expectation
                         max_bid= b
@@ -79,7 +81,49 @@ print(max_exps)
 for L in range(1, N+1):
     for C in range(0, N+1-L):
         for y in range(0, C+1):
-            opt_bid = max_exps[y,C,L]
+            opt_bid = max_bids[y,C,L]
             print("y:", y, " C:", C, " L:", L, ":::", opt_bid, " ")
 
-# ##
+# ##Number 5
+
+
+## Number 6
+N=50
+max_bids= np.zeros((N+1,N+1,N+1))
+max_exps= np.zeros((N+1,N+1,N+1))
+for l in range(1,N+1):
+    for c in range(0,N-l+1):
+        for g in range(0,c+1):
+            max_exp=0
+            max_bid=0
+            x= (alpha+g)/(alpha+beta+c)
+            if (l==0):
+                max_bids[g,c,l]
+            else:
+                for bid in range(0,101,1):
+                    b=bid/10
+                    p1= 1-(b/10)
+                    p3= (b/10)*x
+                    p2= (b/10)*(1-x)
+                    # expectation = p1 + p2*(-b/2) + p3*(10-b/2)
+                    expectation= p1*max_bids[l-1,c,g] + p2*(max_bids[l-1,c+1,g]-b/2) + p3*(max_bids[l-1,c+1,g]+10-b/2)
+                    if expectation > max_exp:
+                        max_exp = expectation
+                        max_bid= b
+                        max_bids[g,c,l]= max_bid
+                        max_exps[g,c,l]= max_exp
+deltas= np.zeros((N+1))
+mu= (alpha)/(alpha+beta)
+for l in range(1, N+1):
+    for c in range(0, N+1-l):
+        for g in range(0, c+1):
+            if (g==0) and (c==0):
+                delta= (max_bids[g,c,l])-(10*mu)
+                print(max_bids[g,c,l])
+                print(delta)
+                deltas[l]= delta
+
+print(deltas)
+print(max_bids)
+plt.plot(deltas)
+plt.show()
