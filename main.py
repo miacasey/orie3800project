e090df37d395af4d69ed92e2b3b6c581b4431e0e
miacasey <miacasey98@outlook.com>
2019-05-06 20:48:42 -0400

    merge

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ N=5
 # bid_dict={}
 # max_expectation=0:
 # for L in range(0,N+1):
-#     for C in range(0, N-L+1): 
-#         for G in range(0, C): 
+#     for C in range(0, N-L+1):
+#         for G in range(0, C):
 #             for bid in range(0,101,1):
 #                 expectation=0
 #                 b=bid/10
@@ -32,43 +32,30 @@ def V(gamma, clicks, left):
             expectation+= p2*(V(gamma,clicks+1,left-1)-b/2)
             expectation+= p3*(V(gamma+1,clicks+1,left-1)+10-b/2)
             bids_exp[b]= expectation
-
     max_exp= max(bids_exp.values())
     ##max_bid=max(bids_exp.items(),  key=operator.itemgetter(1))
     ##print(max_bid)
     return max_exp
 
-##need to keep track of largest expected value 
-##use bottom up version of for loop 
-##just call the largest expected value so far 
+##need to keep track of largest expected value
+##use bottom up version of for loop
+##just call the largest expected value so far
 
-##Number 3 
+##Number 3
 def makeTable(N):
-<<<<<<< HEAD
-    table= np.zeros((N,N,N))
-    for l in range(N,0,-1):
-        for c in range(N-l+1):
-=======
     table= np.zeros((N+1,N+1,N+1))
     for l in range(N+1):
         for c in range(N+1-l):
->>>>>>> 2731ede7063a217d306a19003aa97a4dff16a732
             for g in range(c+1):
                 table[l,g,c]= V(g,c,l)
                 print(V(g,c,l))
     return(table)
 
-<<<<<<< HEAD
-# print(makeTable(N))
-print(V(10,10,2))
-print(V(0,10,2))
-print(V(0,0,5))
-=======
-##Number 4 
-def tabOptBid(N): 
-    for L in range(0, N+1):  
-        for C in range(0, N+1-L): 
-            for y in range(0, C+1): 
+##Number 4
+def tabOptBid(N):
+    for L in range(0, N+1):
+        for C in range(0, N+1-L):
+            for y in range(0, C+1):
                 opt_bid = V(y, C, L)
                 print("y:", y, " C:", C, " L:", L, ":::", opt_bid, " ")
 ##tabOptBid(3)
@@ -76,4 +63,3 @@ def tabOptBid(N):
 print(makeTable(N))
 ##print(V(10,10,2))
 #print(V(0,10,2))
->>>>>>> 2731ede7063a217d306a19003aa97a4dff16a732
